a0e9dd95d6acaa46239c005bc54391cd603e9dab
Juan Camilo Botero Rendon <juancamiloinfor622@gmail.com>
2024-05-28 14:57:22 -0500

    finished

--- app/main.py
+++ app/main.py
@@ -4,8 +4,22 @@ from tkinter import ttk
 from PIL import Image, ImageTk
 from utils import Utils
 
+
 class FileExplorer(tk.Tk):
+
+    """Class File Explorer that inherit the Class tk.Tk. Build the GUI.
+
+        Args:
+
+            tk.TK(): Contains all the tools to the GUI.
+
+        Returns:
+
+            None
+    """
+
     def __init__(self):
+        """init constructor"""
         super().__init__()
 
         self.title("Technical Test Servinformación")
@@ -13,11 +27,13 @@ class FileExplorer(tk.Tk):
 
         # Create the Treeview widget
         self.tree = ttk.Treeview(self)
-        self.tree.heading("#0", text="SEARCH FOR AN IMAGE AN TRY THE MODEL", anchor='w')
+        self.tree.heading(
+            "#0", text="SEARCH FOR AN IMAGE AN TRY THE MODEL", anchor='w')
         self.tree.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-        
+
         # Create a Scrollbar for the Treeview
-        self.tree_scroll = ttk.Scrollbar(self, orient=tk.VERTICAL, command=self.tree.yview)
+        self.tree_scroll = ttk.Scrollbar(
+            self, orient=tk.VERTICAL, command=self.tree.yview)
         self.tree.configure(yscroll=self.tree_scroll.set)
         self.tree_scroll.pack(side=tk.LEFT, fill=tk.Y)
 
@@ -28,13 +44,16 @@ class FileExplorer(tk.Tk):
         self.image_label = ttk.Label(self.image_frame)
         self.image_label.pack(fill=tk.BOTH, expand=True)
 
-        self.title_label = ttk.Label(self.image_frame, text="", font=("Helvetica", 16))
+        self.title_label = ttk.Label(
+            self.image_frame, text="", font=("Helvetica", 16))
         self.title_label.pack(pady=10)
 
-        self.title_predict = ttk.Label(self.image_frame, text="", font=("Helvetica", 16))
+        self.title_predict = ttk.Label(
+            self.image_frame, text="", font=("Helvetica", 16))
         self.title_predict.pack(pady=20)
         # Add a button to the image frame
-        self.button = ttk.Button(self.image_frame, text="Click to Predict!", command=self.on_button_click)
+        self.button = ttk.Button(
+            self.image_frame, text="Click to Predict!", command=self.on_button_click)
         self.button.pack(pady=10)
         self.button.pack_forget()  # Hide button initially
 
@@ -42,12 +61,35 @@ class FileExplorer(tk.Tk):
         self.tree.bind("<<TreeviewSelect>>", self.on_tree_select)
 
     def populate_tree(self):
+        """Function that pops up the folder tree.
+
+        Args:
+
+            None
+
+        Returns:
+
+            None
+    """
+
         path = os.getcwd()
-        self.path = os.path.join(path,'image_test')
+        self.path = os.path.join(path, 'image_test')
         node = self.tree.insert('', 'end', text=self.path, open=True)
         self.process_directory(node, self.path)
 
     def process_directory(self, parent, path):
+        """Function that join and preprocess the paths.
+
+        Args:
+
+            parents(str): Receive the parent on the file path.
+            path(str): Current Path.
+
+        Returns:
+
+            None
+    """
+
         for item in os.listdir(path):
             abs_path = os.path.join(path, item)
             is_dir = os.path.isdir(abs_path)
@@ -57,14 +99,25 @@ class FileExplorer(tk.Tk):
                 self.process_directory(node, abs_path)
 
     def on_tree_select(self, event):
+        """Function that reacts when an event (mouseclick) is performed over the folder tree
+
+        Args:
+
+            event(envet): It receives the event function as a trigger.
+
+        Returns:
+
+            None
+    """
+
         selected_item = self.tree.selection()[0]
         file_path = self.get_full_path(selected_item)
-        
+
         if os.path.isfile(file_path) and self.is_image_file(file_path):
             self.display_image(file_path)
 
             raw_text = os.path.basename(file_path)
-            file_path_splitted=file_path.split('\\')[::-1][2]
+            file_path_splitted = file_path.split('\\')[::-1][2]
             file_path_splitted = f'y_true: {file_path_splitted}'
             self.title_label.config(text=file_path_splitted)
             self.button.pack(pady=10)  # Show button when an image is displayed
@@ -73,35 +126,84 @@ class FileExplorer(tk.Tk):
             self.button.pack_forget()  # Hide button if no image is selected
 
     def get_full_path(self, item):
+        """Join the path with the name of the images.
+
+        Args:
+
+            item(str): Name of the image
+
+        Returns:
+
+            None
+    """
+
         path = self.tree.item(item, 'text')
         parent = self.tree.parent(item)
         while parent:
-            path = os.path.join(self.tree.item(parent, 'text'),path)
+            path = os.path.join(self.tree.item(parent, 'text'), path)
             self.path_final = path
             parent = self.tree.parent(parent)
         return path
 
     def is_image_file(self, file_path):
+        """Check if the last joined path is an image.
+
+        Args:
+
+            file_path(str): Current path.
+
+        Returns:
+
+            None
+    """
+
         valid_extensions = ('.png', '.jpg', '.jpeg', '.gif', '.bmp')
         return file_path.lower().endswith(valid_extensions)
 
     def display_image(self, file_path):
+        """Pops up the image that has been selected
+
+        Args:
+
+            file_path(str): Current path were the image is located
+
+        Returns:
+
+            None
+    """
+
         image = Image.open(file_path)
-        image = image.resize((400, 400), Image.LANCZOS)  # Resize for display purposes
+        # Resize for display purposes
+        image = image.resize((400, 400), Image.LANCZOS)
         photo = ImageTk.PhotoImage(image)
         self.image_label.config(image=photo)
         self.image_label.image = photo
 
     def on_button_click(self):
-        label_dict = {0: 'Avulsion fracture', 1: 'Comminuted fracture', 2: 'Fracture Dislocation', 3: 'Greenstick fracture', 4: 'Hairline Fracture', 5: 'Impacted fracture', 6: 'Longitudinal fracture', 7: 'Oblique fracture', 8: 'Pathological fracture', 9: 'Spiral Fracture'}
+        """When the button it clicked event taken
+
+        Args:
+
+            None
+
+        Returns:
+
+            None
+    """
+
+        label_dict = {0: 'Avulsion fracture', 1: 'Comminuted fracture', 2: 'Fracture Dislocation', 3: 'Greenstick fracture', 4: 'Hairline Fracture',
+                      5: 'Impacted fracture', 6: 'Longitudinal fracture', 7: 'Oblique fracture', 8: 'Pathological fracture', 9: 'Spiral Fracture'}
         IMG_PATH = self.path_final
         MODEL_PATH = r'D:\Users\user\Desktop\prueba-tecnica-Servinformacion\model\BestCNNModel.keras'
 
-        ut = Utils(MODEL_PATH,IMG_PATH,label_dict)
+        ut = Utils(MODEL_PATH, IMG_PATH, label_dict)
         prediction = ut.predict()
         if prediction:
             text = f'y_pred: {prediction}'
-            self.title_predict.config(text= text)
+            self.title_predict.config(text=text)
+
+
 if __name__ == "__main__":
+    """Create FileExplorer instance"""
     app = FileExplorer()
     app.mainloop()
--- app/utils.py
+++ app/utils.py
@@ -3,26 +3,77 @@ import numpy as np
 
 
 class Utils():
+    """Utils class that perform the call an predict to the model
 
-    def __init__(self,path_model:str,
-                 path:str,
-                 label_dic:str):
-        
+        Args:
+
+            None
+
+        Returns:
+
+            None
+    """
+
+    def __init__(self, path_model: str,
+                 path: str,
+                 label_dic: str):
+        """Init Constructor.
+
+        Args:
+
+            path_model(str): Receive path_model, can alternate between the 2 possible models.
+            path:(str): Receive image path.
+            label_dic(dict): Diccionary with the labels
+
+        Returns:
+
+            None
+        """
         self.path_model = path_model
         self.path = path
         self.label_dic = label_dic
-    
+
     def load_model(self):
+        """Load the model using tf instance
+
+        Args:
+
+            None
+
+        Returns:
+
+            tf.keras.models.load_model(self.path_model)
+        """
         return tf.keras.models.load_model(self.path_model)
 
     def load_img(self):
-        img=tf.keras.utils.load_img(self.path,target_size = (256,256))
-        img=tf.keras.utils.img_to_array(img) / 255.0
-        img = np.expand_dims(img,axis = 0)
-    
+        """Load the image using tf instance
+
+        Args:
+
+            None
+
+        Returns:
+
+            img(np.ndarray): An array with the correct shape to the model.
+        """
+        img = tf.keras.utils.load_img(self.path, target_size=(256, 256))
+        img = tf.keras.utils.img_to_array(img) / 255.0
+        img = np.expand_dims(img, axis=0)
+
         return img
 
     def predict(self):
+        """Classify the label
+
+        Args:
+
+            None
+
+        Returns:
+
+            self.label_dic[y_pred](str): Classification done by the model.
+        """
         model = self.load_model()
         img = self.load_img()
         prediction = model.predict(img)
@@ -30,19 +81,12 @@ class Utils():
         return self.label_dic[y_pred]
 
 
+if __name__ == '__main__':
 
-
-
-
-if __name__ =='__main__':
-        
-        label_dict = {0: 'Avulsion fracture', 1: 'Comminuted fracture', 2: 'Fracture Dislocation', 3: 'Greenstick fracture', 4: 'Hairline Fracture', 5: 'Impacted fracture', 6: 'Longitudinal fracture', 7: 'Oblique fracture', 8: 'Pathological fracture', 9: 'Spiral Fracture'}
-        PATH = r'./model/cnn_last_try.keras'
-        IMG_PATH = r'./image_test\Avulsion fracture/Test/1-s2-0-S0899707114002836-gr2_jpg.rf.bb8ed4dee892edbb760f6ce687d74ba5.jpg'
-        ut = Utils(PATH,IMG_PATH,label_dict)
-        prediction = ut.predict()
-        print(prediction)
-       
-        
-        
-
+    label_dict = {0: 'Avulsion fracture', 1: 'Comminuted fracture', 2: 'Fracture Dislocation', 3: 'Greenstick fracture', 4: 'Hairline Fracture',
+                  5: 'Impacted fracture', 6: 'Longitudinal fracture', 7: 'Oblique fracture', 8: 'Pathological fracture', 9: 'Spiral Fracture'}
+    PATH = r'./model/cnn_last_try.keras'
+    IMG_PATH = r'./image_test\Avulsion fracture/Test/1-s2-0-S0899707114002836-gr2_jpg.rf.bb8ed4dee892edbb760f6ce687d74ba5.jpg'
+    ut = Utils(PATH, IMG_PATH, label_dict)
+    prediction = ut.predict()
+    print(prediction)
